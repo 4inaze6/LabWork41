01e9bcfa93623e5a0baecca25b78211b398a2f9a
mzrt <vdksemaluv@icloud.com>
2024-05-26 19:19:07 +0300

    изменен проект Task1

--- Task1/MainWindow.xaml.cs
+++ Task1/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -16,9 +17,45 @@ namespace Task1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool menuIsOpen = false;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            menuIsOpen = !menuIsOpen;
+
+            DoubleAnimation MenuAnimation = new DoubleAnimation
+            {
+                To = menuIsOpen ? 100 : 50,
+                Duration = TimeSpan.FromSeconds(1),
+            };
+
+            DoubleAnimation StackPanelAnimation = new DoubleAnimation
+            {
+                To = menuIsOpen ? 80 : 0,
+                Duration = TimeSpan.FromSeconds(1),
+            };
+
+            DoubleAnimation ButtonWidthAnimation = new DoubleAnimation
+            {
+                To = menuIsOpen ? 80 : 40,
+                Duration = TimeSpan.FromSeconds(1),
+            };
+
+            DoubleAnimation ButtonHeightAnimation = new DoubleAnimation
+            {
+                To = menuIsOpen ? 80 : 40,
+                Duration = TimeSpan.FromSeconds(1),
+            };
+
+            MainMenu.BeginAnimation(WidthProperty, MenuAnimation);
+            BurgerStackPanel.BeginAnimation(WidthProperty, StackPanelAnimation);
+            BurgerButton.BeginAnimation(WidthProperty, ButtonWidthAnimation);
+            BurgerButton.BeginAnimation(HeightProperty, ButtonHeightAnimation);
+
         }
     }
 }
